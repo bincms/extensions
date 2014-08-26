727a8db064de0666c9ab21680f2b113963d43b15
Vagrant User <vagrant@packer-virtualbox-iso.vagrantup.com>
2014-08-26 10:59:15 +0000

    new extension

--- shop.js
+++
@@ -1,9 +0,0 @@
-{
-    "name": "Shop Extension",
-    "description": "Extension description",
-    "download": {
-        "url": "https://github.com/bincms/Shop.git",
-        "version": "master",
-        "type": "git"
-    }
-}
